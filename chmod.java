c043e31b4d75bc2c05ee241afdf904523f1e775f
maxVeremchuk <maxverem12398@gmail.com>
2019-03-27 18:44:16 +0200

    first commit

---
+++ chmod.java
@@ -0,0 +1,27 @@
+public class chmod {
+    private static String PROGRAM_NAME = "chomd";
+    public static void main (String[] args) throws Exception {
+        Kernel.initialize();
+        if (args.length < 2) {
+            Kernel.perror( "USAGE: chown <mode> <entry_names>...");
+            Kernel.exit( 1 );
+        }
+        short mode = Short.valueOf(args[0]);
+        for(int i = 1; i < args.length; i++){
+            if(mode < 0 || mode > 777){
+                Kernel.perror( PROGRAM_NAME );
+                System.err.println( PROGRAM_NAME +
+                        ": unable to set mode with " + mode + " value" );
+                Kernel.exit( 1 );
+            }
+            int status = Kernel.chmod(args[i], mode);
+            if(status < 0){
+                Kernel.perror( PROGRAM_NAME );
+                System.err.println( PROGRAM_NAME +
+                        ": unable to find inode" );
+                Kernel.exit( 1 );
+            }
+        }
+        Kernel.exit(0);
+    }
+}
---
+++ chown.java
@@ -0,0 +1,21 @@
+public class chown {
+    private static String PROGRAM_NAME = "chown";
+    public static void main (String[] args) throws Exception {
+        Kernel.initialize();
+        if (args.length < 2) {
+            Kernel.perror( "USAGE: chown <uid_number> <entry_names>...");
+            Kernel.exit( 1 );
+        }
+        short uid = Short.valueOf(args[0]);
+        for(int i = 1; i < args.length; i++){
+            int status = Kernel.chown(args[i], uid);
+            if(status < 0){
+                Kernel.perror( PROGRAM_NAME );
+                System.err.println( PROGRAM_NAME +
+                        ": unable to find inode" );
+                Kernel.exit( 1 );
+            }
+        }
+        Kernel.exit(0);
+    }
+}
