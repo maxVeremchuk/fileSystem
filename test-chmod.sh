9e444c5d836f991560ff9e4a6c7655c6b61f0dc2
AndrewKhoma <AndrewKhomaOfficial@gmail.com>
2019-03-31 10:24:33 +0300

    upd test

--- test-chmod.sh
+++ test-chmod.sh
@@ -6,7 +6,8 @@ javac *.java
 for i in {0..7..1}; do
   for j in {0..7..1}; do
     for k in {0..7..1}; do
-      java chmod $i$j$k /usr
+      java chmod $i$j$k /usr       # test dir
+      java chmod $i$j$k /text.txt  # regular file
     done
   done
  done
